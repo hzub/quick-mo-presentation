626874c65b8d73c626a30d35a9335465f809272a
Hubert Zub <hubert.zub@gmail.com>
2017-03-05 19:25:13 +0100

    First mutation observer

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ angular
     $scope.elements.push(`Element ${$scope.elements.length + 1}`);
   };
 });
+
+
+// ----------------------------
+
+function onMutateCallback(mutationList) {
+
+  mutationList.forEach(function(mutation) {
+    console.log('====== Mutation ========');
+    console.log('Type: ', mutation.type);
+    console.log('Added nodes: ', mutation.addedNodes);
+    console.log('Target: ', mutation.target);
+  });
+
+}
+
+const ourContainer = document.querySelector('.container');
+const ourMutationObserver = new MutationObserver(onMutateCallback);
+
+ourMutationObserver.observe(ourContainer, {
+  attributes: true,
+  childList: true,
+  characterData: true,
+});
